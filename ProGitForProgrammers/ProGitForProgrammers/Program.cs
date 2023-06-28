edd5af4fc0c2a70ed69e545d881018d873e02a3f
86868 <flakcajty@gmail.com>
2023-06-28 08:03:36 +0800

    my message

--- ProGitForProgrammers/ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/ProGitForProgrammers/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("I Just Add This");
+Console.WriteLine("This Line add to show he staging area");
